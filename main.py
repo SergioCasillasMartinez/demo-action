4706152177094dd5a0db37a9e63532d88c8e72b7
Sergio Casillas Martinez <sercasmar01@gmail.com>
2024-01-21 18:29:19 +0100

    Añadimos funciones y test de comprobacion para las funciones

---
+++ main.py
@@ -0,0 +1,14 @@
+def suma(x: int, y: int):
+    return x + y
+
+def resta(x: int, y: int):
+    return x - y
+
+def multiplicacion(x: int, y: int):
+    return x * y
+
+def division(x: int, y: int):
+    if (x != 0 or y != 0):
+        return x / y
+    else:
+        return 0
---
+++ test_main.py
@@ -0,0 +1,24 @@
+import unittest
+from main import suma,resta,multiplicacion,division
+
+class Test(unittest.TestCase):
+    
+    def test(self):
+        self.assertEqual(suma(3,2), 5)
+        self.assertEqual(suma(-1,1), 0)
+        self.assertEqual(suma(-1,-1), -2)
+
+        self.assertEqual(resta(1,2), 1)
+        self.assertEqual(resta(5,2), 3)
+        self.assertEqual(resta(1,2), -1)
+
+        self.assertEqual(multiplicacion(10,5), 50)
+        self.assertEqual(multiplicacion(3,3), 9)
+        self.assertEqual(multiplicacion(3,2), 6)
+
+        self.assertEqual(division(1,1), 1)
+        self.assertEqual(division(25,5), 5)
+        self.assertEqual(division(5,0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
